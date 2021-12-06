93519a690edffe23638fb4880985297bcc495ec5
guilhermeabel99 <gpeuvis@gmail.com>
2021-12-05 23:45:40 -0300

    ADD: webpack config #4

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+const { resolve } = require('path');
+const path = require('path');
+
+module.exports = {
+  entry: path.resolve(__dirname, 'src', 'index.jsx'),
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  resolve: {
+    extensions: ['.js', '.jsx']
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx$/,
+        exclude: /node_modules/,
+        use: 'babel-loader',
+      }
+    ],
+  }
+};
