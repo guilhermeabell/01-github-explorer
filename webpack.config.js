08dadcdd07721dea4fca517f70c30dd92d65a298
guilhermeabel99 <gpeuvis@gmail.com>
2021-12-06 00:12:21 -0300

    ADD: webpack server #6

--- webpack.config.js
+++ webpack.config.js
@@ -12,6 +12,10 @@ module.exports = {
   resolve: {
     extensions: ['.js', '.jsx']
   },
+
+  devServer: {
+    static: path.resolve(__dirname, 'public')
+  },
   plugins: [
     new HtmlWebpackPlugin({
       template: path.resolve(__dirname, 'public', 'index.html' )
