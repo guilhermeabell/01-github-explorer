1d75711af1646d10372ea36291329913cf38aea6
guilhermeabel99 <gpeuvis@gmail.com>
2021-12-06 00:18:22 -0300

    ADD: source maps webpack #7

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
 export function App() {
+  throw new Error('error')
+
   return <h1>Hello ReactJS</h1>
 }
--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   mode: 'development',
+  devtool: 'eval-source-map',
   entry: path.resolve(__dirname, 'src', 'index.jsx'),
   output: {
     path: path.resolve(__dirname, 'dist'),
@@ -12,7 +13,6 @@ module.exports = {
   resolve: {
     extensions: ['.js', '.jsx']
   },
-
   devServer: {
     static: path.resolve(__dirname, 'public')
   },
