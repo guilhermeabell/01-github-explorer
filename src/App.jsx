46a325ece370389b61f4688c51072dffbfe47cae
guilhermeabel99 <gpeuvis@gmail.com>
2021-12-06 10:02:45 -0300

    ADD: first component, repository list #13

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
-import './styles/global.scss';
+import { RepositoryList } from './components/RepositoryList'
+import './styles/global.scss'
+
 
 export function App() {
-
-  return <h1>Hello ReactJS</h1>
+  return <RepositoryList />
 }
---
+++ src/components/RepositoryList.jsx
@@ -0,0 +1,37 @@
+export function RepositoryList() {
+  return(
+    <section className="repository-list">
+      <h1>
+        Lista de repositórios
+      </h1>
+      <ul>
+        <li>
+          <strong>Unform</strong>
+          <p>Forms in ReactJS</p>
+
+          <a href="">
+            Acessar repositório
+          </a>
+        </li>
+
+        <li>
+          <strong>Unform</strong>
+          <p>Forms in ReactJS</p>
+
+          <a href="">
+            Acessar repositório
+          </a>
+        </li>
+
+        <li>
+          <strong>Unform</strong>
+          <p>Forms in ReactJS</p>
+
+          <a href="">
+            Acessar repositório
+          </a>
+        </li>
+      </ul>
+    </section>
+  )
+}
