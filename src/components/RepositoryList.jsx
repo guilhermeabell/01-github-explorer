17543bbb2f371204f6f344a15afce0e65c918ce6
vulgoguix <gpeuvis@gmail.com>
2022-03-22 14:15:38 -0300

    alteracao de repo

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -3,32 +3,25 @@ import { RepositoryItem } from "./RepositoryItem";
 
 import '../styles/repositories.scss'
 
-// api.github/users/vulgoguix
-
-const repository = {
-  name: 'unform',
-  description: 'Forms in React',
-  link: 'https://github.com/guilhermeabel99'
-}
 
 export function RepositoryList() {
   const [repositories, setRepositories] = useState([]);
 
   useEffect(() => {
-      fetch('https://api.github.com/users/vulgoguix')
+      fetch('https://api.github.com/orgs/rocketseat/repos')
       .then( response => response.json())
       .then( data => setRepositories(data))
-  }, [repositories])
+  }, [])
   
   return ( 
     <section className="repository-list">
       <h1>Lista de repositórios</h1>
 
       <ul>
-        <RepositoryItem repository={repository} />
-        <RepositoryItem repository={repository} />
-        <RepositoryItem repository={repository} />
-        <RepositoryItem repository={repository} />
+        {repositories.map(repository => {
+          return <RepositoryItem key={repository.name} repository={repository} />
+        } )}
+        
       </ul>
     </section>
   )
